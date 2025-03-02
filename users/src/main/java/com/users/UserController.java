c82c51f0c5b624a97ec7d705cad57eab0392825e
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-03-02 16:34:16 +0100

    Created users microservice

---
+++ users/src/main/java/com/users/UserController.java
@@ -0,0 +1,17 @@
+package com.users;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "Works!!!";
+    }
+}
