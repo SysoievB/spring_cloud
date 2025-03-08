647318fbb32707f6249c81249e3fa6c9d1e46405
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-03-08 06:07:40 +0100

    Added security & DB to users microservice

--- users/src/main/java/com/users/controller/UserController.java
+++ users/src/main/java/com/users/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @Validated
