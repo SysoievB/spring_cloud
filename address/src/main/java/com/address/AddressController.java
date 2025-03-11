abc7e99c77c229abb3f2fc46aaefaa17509e3456
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-03-11 05:58:44 +0100

    Added settings for address microservice

--- address/src/main/java/com/address/AddressController.java
+++ address/src/main/java/com/address/AddressController.java
@@ -1,15 +1,15 @@
-package com.account;
+package com.address;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/accounts")
-public class AccountController {
+@RequestMapping("/address")
+public class AddressController {
 
     @GetMapping("/status/check")
-    public String status() {
-        return "Accounts Works!!!";
+    public String checkStatus() {
+        return "Address works!!!";
     }
 }
