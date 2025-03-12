87dff45e735a7c2afb26897908d2c9d49b4e692e
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-03-12 19:35:50 +0100

    Added resilience4j for users microservice

--- users/src/main/java/com/users/client/AddressesFeignClient.java
+++ users/src/main/java/com/users/client/AddressesFeignClient.java
@@ -1,14 +1,21 @@
 package com.users.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @FeignClient(name = "address", path = "/address")
 public interface AddressesFeignClient {
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "address", fallbackMethod = "")
     Optional<AddressDto> getAddress(@PathVariable("id") Long id);
+
+    default Optional<AddressDto> getAddressFallback(Long id, Throwable throwable) {
+        return Optional.empty();
+    }
 }
