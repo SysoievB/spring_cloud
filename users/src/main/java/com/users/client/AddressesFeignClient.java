22ad576a46637f3870fea01e9c0b11741be20967
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-03-13 06:25:39 +0100

    Added retry

--- users/src/main/java/com/users/client/AddressesFeignClient.java
+++ users/src/main/java/com/users/client/AddressesFeignClient.java
@@ -1,21 +1,23 @@
 package com.users.client;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Collection;
 import java.util.Optional;
 
 @FeignClient(name = "address", path = "/address")
 public interface AddressesFeignClient {
 
     @GetMapping("/{id}")
-    @CircuitBreaker(name = "address", fallbackMethod = "")
+    @Retry(name = "address")
+    @CircuitBreaker(name = "address", fallbackMethod = "getAddressFallback")
     Optional<AddressDto> getAddress(@PathVariable("id") Long id);
 
     default Optional<AddressDto> getAddressFallback(Long id, Throwable throwable) {
+        System.out.println("ID=" + id + ", FALLBACK=" + throwable.getMessage());
         return Optional.empty();
     }
 }
